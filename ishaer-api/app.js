11c038816f9d04ad7b33113b5b0f2388325fb033
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-01-24 10:09:22 -0400

    Enabling use of temp files.

--- ishaer-api/app.js
+++ ishaer-api/app.js
@@ -1,14 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const fileUpload = require('express-fileupload');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +27,10 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(fileUpload({
+  useTempFiles: true
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
