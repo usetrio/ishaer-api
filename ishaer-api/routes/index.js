76f71c61c661f586bbe839c462292bf79a6bdae1
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-01-24 10:07:44 -0400

    Creation of cloudinary configuration and required consts.

--- ishaer-api/routes/index.js
+++ ishaer-api/routes/index.js
@@ -1,5 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const formidable = require('formidable');
+const cloudinary = require('cloudinary');
+
+require('dotenv').config();
+
+cloudinary.config({
+  cloudName: process.env.CLOUD_NAME,
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
