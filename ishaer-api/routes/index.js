8e3a77510862135fa0a4058d93f337d11e0ed066
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-01-24 11:18:59 -0400

    Adding cloudinary upload code.

--- ishaer-api/routes/index.js
+++ ishaer-api/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const formidable = require('formidable');
-const cloudinary = require('cloudinary');
+const cloudinary = require('cloudinary').v2;
 
 require('dotenv').config();
 
 cloudinary.config({
-  cloudName: process.env.CLOUD_NAME,
-  apiKey: process.env.API_KEY,
-  apiSecret: process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 })
 
 /* GET home page. */
@@ -16,14 +16,38 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'iShaer' });
 });
 
+/* POST upload asset endpoint. */
 router.post('/upload', (req, res, next) => {
   const asset = req.files.asset;
   console.log(asset);
 
-  res.send({
-    success: true,
-    message: 'File ready!'
-  })
-})
+  /* Take the name of the asset spliting the string by the last dot getting the extension. */
+  let assetTitle = asset.name.split(/\.(?=[^\.]+$)/)[0];
+
+  /* Upload asset to Cloudinary */
+  cloudinary.uploader.upload(asset.tempFilePath, (err, result) => {
+    if(err) {
+      console.log(err);
+
+      res.render('error', { 
+        message: 'An error ocurred!',
+        err
+      });
+
+      res.send({
+        success: false,
+        error: err
+      });
+    }
+    else {
+      console.log(result);
+
+      res.send({
+        success: true,
+        message: 'File ready!'
+      });
+    }
+  });
+});
 
 module.exports = router;
